4903b10bda020ecec3df51ac1bb9b85377b60659
perillamint <perillamint@quendi.moe>
2018-09-10 19:47:57 +0900

    BIP fontrom packer

--- js/bipfont.js
+++ js/bipfont.js
@@ -50,12 +50,14 @@ class BIPFont {
         for (const elem of ranges) {
             for (let i = elem.start; i <= elem.end; i++) {
                 const font = bin.slice(off, off + 32);
-                const margin_top = bin[off + 32] % 16;
+                const margin_top = bin[off + 32] & 0x0F;
+                const width = bin[off + 32] >> 4;
                 off += 33;
 
                 fonts[i] = {
-                    font: font,
+                    data: font,
                     margin_top: margin_top,
+                    width: width,
                 };
             }
         }
@@ -69,11 +71,43 @@ class BIPFont {
             const bn = parseInt(b);
 
             return an < bn ? -1 : (an > bn ? 1 : 0);
+        }).map((x) => {
+            return parseInt(x);
         });
 
-        for (const i of codepoints) {
-            //
+        let header = Buffer.from('4E455A4B08FFFFFFFFFF01000000FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF0000', 'hex');
+        let body = Buffer.from([]);
+
+        let blkstart = codepoints[0];
+        let blkcnt = 0;
+        let seqcnt = 0;
+        for (let i = 0; i < codepoints.length; i++) {
+            const font = fontmap[codepoints[i]];
+
+            if (font.data.length != 32) {
+                throw new Error('Invalid font binary');
+            }
+
+            const padding = ((font.width << 4) + (font.margin_top & 0x0F)) & 0xFF;
+            const fontimg = Buffer.concat([font.data, Buffer.from([padding])]);
+            body = Buffer.concat([body, fontimg]);
+
+            if (codepoints[i + 1] == null || codepoints[i] + 1 !== codepoints[i + 1]) {
+                // End of continuous block
+                blkcnt ++;
+                const blkEntry = Buffer.alloc(6);
+                blkEntry.writeUInt16LE(blkstart, 0);
+                blkEntry.writeUInt16LE(codepoints[i], 2);
+                blkEntry.writeUInt16LE(seqcnt, 4);
+                header = Buffer.concat([header, blkEntry]);
+                seqcnt += codepoints[i] - blkstart + 1;
+                blkstart = codepoints[i + 1] != null ? codepoints[i + 1] : -1;
+            }
         }
+
+        header.writeUInt16LE(blkcnt, 0x20);
+
+        return Buffer.concat([header, body]);
     }
 }
 
