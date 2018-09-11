b9637a521f271bd00560e4056bbabf2e4ddfd5fb
perillamint <perillamint@quendi.moe>
2018-09-11 13:44:05 +0900

    Bitmap storage

---
+++ src/bitmapstorage.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const bipFont = require('./bipfont.js');
+
+class BitmapStorage {
+    constructor() {
+        this._bitmaps = {};
+    }
+
+    static fromBIPFont(buffer) {
+        fontmap = bipFont.unpackFile(buffer);
+        const bms = new BitmapStorage();
+        bms.setBitmaps(fontmap);
+
+        return bms;
+    }
+
+    setBitmaps(bitmapobj) {
+        this._bitmaps = bitmapobj;
+    }
+
+    addGlyph(code, bitmap, margin_top, width) {
+        this._bitmaps[code] = {
+            data: bitmap,
+            margin_top: margin_top,
+            width: width,
+        };
+    }
+
+    buildBipFont() {
+        return bipFont.packFile(this._bitmaps);
+    }
+}
