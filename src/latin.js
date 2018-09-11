5374dd60de74bba196097cf51a8dfbbae792b121
perillamint <perillamint@quendi.moe>
2018-09-11 11:35:26 +0900

    Latin font renderer

---
+++ src/latin.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const FontRenderer = require('./fontrenderer.js');
+
+class LatinRenderer extends FontRenderer {
+    constructor(font, width, height) {
+        super(width, height);
+
+        this._fontbin = font;
+        this._fonts = [];
+
+        for (let i = 0; i < 0xFF; i++) {
+            this._fonts[i] = this._grabChar(i);
+        }
+    }
+
+    _grabChar(off) {
+        const {width, height} = this.getDimension();
+        const byteWidth = this.getByteWidth();
+        const characterByte = byteWidth * height;
+
+        return this._fontbin.slice(characterByte * off, characterByte * (off + 1));
+    }
+
+    renderChar(code) {
+        const {width, height} = this.getDimension();
+        const byteWidth = this.getByteWidth();
+        const characterByte = byteWidth * height;
+
+        const buf = Buffer.alloc(32);
+
+        const rowStep = byteWidth === 2 ? 1 : 2;
+        for (let i = 0; i < characterByte; i++) {
+            buf[i * rowStep] |= this._fonts[code][i];
+        }
+
+        return buf;
+    }
+}
+
+module.exports = LatinRenderer;
