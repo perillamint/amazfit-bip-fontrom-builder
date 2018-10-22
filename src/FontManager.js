eacd55ec504d4363322cb0e0801e5fc44b16e016
perillamint <perillamint@quendi.moe>
2018-10-22 12:33:39 +0900

    Async font loading support

--- src/FontManager.js
+++ src/FontManager.js
@@ -29,38 +29,34 @@ class FontManager {
         const vendorfntmeta = (await axios.get('./asset/vendor/fonts.json')).data;
         const vendorfonts = Object.keys(vendorfntmeta);
         for (const i of vendorfonts) {
-            const fontbin = Buffer.from((await axios.get(`./asset/vendor/${i}`, {responseType: 'arraybuffer'})).data);
-            this.addFont('vendor', fontbin, i, vendorfntmeta[i].name);
+            this.addFont('vendor', `./asset/vendor/${i}`, i, vendorfntmeta[i].name);
         }
 
         const latinfntmeta = (await axios.get('./asset/latin/fonts.json')).data;
         const latinfonts = Object.keys(latinfntmeta);
 
         for (const i of latinfonts) {
-            const fontbin = Buffer.from((await axios.get(`./asset/latin/${i}`, {responseType: 'arraybuffer'})).data);
             const headersz = latinfntmeta[i].header;
             const width = latinfntmeta[i].width;
             const height = latinfntmeta[i].height;
-            this.addFont('latin', fontbin, i, latinfntmeta[i].name, headersz, width, height);
+            this.addFont('latin', `./asset/latin/${i}`, i, latinfntmeta[i].name, headersz, width, height);
         }
 
         const dkb844fntmeta = (await axios.get('./asset/dkb844/fonts.json')).data;
         const dkb844fonts = Object.keys(dkb844fntmeta);
 
         for (const i of dkb844fonts) {
-            const fontbin = Buffer.from((await axios.get(`./asset/dkb844/${i}`, {responseType: 'arraybuffer'})).data);
             const headersz = dkb844fntmeta[i].header;
             const width = dkb844fntmeta[i].width;
             const height = dkb844fntmeta[i].height;
-            this.addFont('dkb844', fontbin, i, dkb844fntmeta[i].name, headersz, width, height);
+            this.addFont('dkb844', `./asset/dkb844/${i}`, i, dkb844fntmeta[i].name, headersz, width, height);
         }
 
         const fontxfntmeta = (await axios.get('./asset/fontx/fonts.json')).data;
         const fontxfonts = Object.keys(fontxfntmeta);
 
         for (const i of fontxfonts) {
-            const fontbin = Buffer.from((await axios.get(`./asset/fontx/${i}`, {responseType: 'arraybuffer'})).data);
-            this.addFont('fontx', fontbin, i, fontxfntmeta[i].name);
+            this.addFont('fontx', `./asset/fontx/${i}`, i, fontxfntmeta[i].name);
         }
     }
 
@@ -70,11 +66,20 @@ class FontManager {
 
     async addFont(type, file, filename, name, headersz, width, height) {
         let renderer = null; // allocate renderer
+
+        const fontBinFetcher = async () => {
+            if (file instanceof Buffer) {
+                return file;
+            } else {
+                return Buffer.from((await axios.get(file, {responseType: 'arraybuffer'})).data);
+            }
+        };
+
         switch (type) {
         case 'vendor':
             this._fonts.vendor.push({
                 getBinary: async () => {
-                    return file;
+                    return await fontBinFetcher();
                 },
                 filename: filename,
                 name: name,
@@ -84,7 +89,7 @@ class FontManager {
             this._fonts.latin.push({
                 getRenderer: async () => {
                     if (renderer == null) {
-                        renderer = new LatinFontRenderer(file, headersz, width, height);
+                        renderer = new LatinFontRenderer(await fontBinFetcher(), headersz, width, height);
                     }
                     return renderer;
                 },
@@ -96,7 +101,7 @@ class FontManager {
             this._fonts.dkb844.push({
                 getRenderer: async () => {
                     if (renderer == null) {
-                        renderer = new Dkb844FontRenderer(file, headersz, width, height);
+                        renderer = new Dkb844FontRenderer(await fontBinFetcher(), headersz, width, height);
                     }
                     return renderer;
                 },
@@ -108,7 +113,7 @@ class FontManager {
             this._fonts.fontx.push({
                 getRenderer: async () => {
                     if (renderer == null) {
-                        renderer = new FontXFontRenderer(file);
+                        renderer = new FontXFontRenderer(await fontBinFetcher());
                     }
                     return renderer;
                 },
